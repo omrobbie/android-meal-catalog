8bbb1350c8ba48735c012ddf837bb43fd0f28fd0
omrobbie <putu.roby@yahoo.com>
2018-12-08 16:50:30 +0700

    pasang service untuk get data

--- app/src/main/java/com/omrobbie/mealcatalog/network/ApiService.kt
+++ app/src/main/java/com/omrobbie/mealcatalog/network/ApiService.kt
@@ -1,4 +1,12 @@
 package com.omrobbie.mealcatalog.network
 
+import retrofit2.http.GET
+import retrofit2.http.Query
+
 interface ApiService {
+
+    @GET("filter.php")
+    fun requestHomeMeals(
+            @Query("a") area: String
+    )
 }
