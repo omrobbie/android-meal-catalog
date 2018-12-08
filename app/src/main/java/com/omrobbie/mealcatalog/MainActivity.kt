c41304052f49d29aea8caf862b53c29ea747ac93
omrobbie <putu.roby@yahoo.com>
2018-12-08 16:07:24 +0700

    update layout, tampilkan ke layar

--- app/src/main/java/com/omrobbie/mealcatalog/MainActivity.kt
+++ app/src/main/java/com/omrobbie/mealcatalog/MainActivity.kt
@@ -2,11 +2,22 @@ package com.omrobbie.mealcatalog
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        initListofMeal()
+    }
+
+    private fun initListofMeal() {
+        val adapter = MealAdapter(this)
+
+        rv_list.adapter = adapter
+        rv_list.layoutManager = LinearLayoutManager(this)
     }
 }
